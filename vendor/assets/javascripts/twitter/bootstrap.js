1e340b6e0704e28dd905e7a9cda57eba59907479
Adam Jones <adam@carnivallabs.com>
2012-01-25 14:40:54 +1300

    popover javascript ordering

--- vendor/assets/javascripts/twitter/bootstrap.js
+++ vendor/assets/javascripts/twitter/bootstrap.js
@@ -4,12 +4,12 @@
 //= require twitter/bootstrap/bootstrap-collapse
 //= require twitter/bootstrap/bootstrap-dropdown
 //= require twitter/bootstrap/bootstrap-modal
+//= require twitter/bootstrap/bootstrap-tooltip
 //= require twitter/bootstrap/bootstrap-popover
 //= require twitter/bootstrap/bootstrap-scrollspy
 //= require twitter/bootstrap/bootstrap-tab
-//= require twitter/bootstrap/bootstrap-tooltip
 //= require twitter/bootstrap/bootstrap-transition
-//= require twitter/bootstrap/bootstrap-typeahead
+//= require twitter/bootstrap/bootstrap-alert
 
 
 
