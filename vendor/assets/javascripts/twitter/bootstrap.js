510f382e32ccba6f14b979a1eb315919cff69c94
Adam Jones <adam@carnivallabs.com>
2012-01-25 14:28:20 +1300

    correct javascript includes

--- vendor/assets/javascripts/twitter/bootstrap.js
+++ vendor/assets/javascripts/twitter/bootstrap.js
@@ -1,8 +1,15 @@
-//= require twitter/bootstrap/alerts
-//= require twitter/bootstrap/buttons
-//= require twitter/bootstrap/dropdown
-//= require twitter/bootstrap/modal
-//= require twitter/bootstrap/twipsy
-//= require twitter/bootstrap/popover
-//= require twitter/bootstrap/scrollspy
-//= require twitter/bootstrap/tabs
+//= require twitter/bootstrap/bootstrap-alert
+//= require twitter/bootstrap/bootstrap-button
+//= require twitter/bootstrap/bootstrap-carousel
+//= require twitter/bootstrap/bootstrap-collapse
+//= require twitter/bootstrap/bootstrap-dropdown
+//= require twitter/bootstrap/bootstrap-modal
+//= require twitter/bootstrap/bootstrap-popover
+//= require twitter/bootstrap/bootstrap-scrollspy
+//= require twitter/bootstrap/bootstrap-tab
+//= require twitter/bootstrap/bootstrap-tooltip
+//= require twitter/bootstrap/bootstrap-transition
+//= require twitter/bootstrap/bootstrap-typeahead
+
+
+
